8dc07830ce1b85ecb00f091b06efc81bee17e24f
Arthur Sun <arthursun2005@gmail.com>
2018-10-22 08:08:08 +1100

    Initial Commit

---
+++ Fluid/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fluid
+//
+//  Created by Arthur Sun on 10/22/18.
+//  Copyright © 2018 Arthur Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
